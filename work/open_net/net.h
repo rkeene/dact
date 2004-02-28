71133dac9b03dd2ffd8fb9541f479f251ed3e201
rkeene <rkeene@noemail.net>
2004-02-28 18:45:24 +0000

    Deleted some stuff from the "work" directory
    FossilOrigin-Name: 530c62de022777bea5aaacbb50bf8531a77c6ca1

--- work/open_net/net.h
+++
@@ -1,3 +0,0 @@
-int createlisten(int port);
-void closeconnection(int sockfd);
-int createconnection_tcp(char *host, int port);
--- work/test/meep.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-int main(void) {
-	char ch;
-	read(STDIN_FILENO, &ch, 1);
-if (lseek(STDIN_FILENO, -1, SEEK_CUR) == -1) printf("Cannot seek on stdin!\n");
-}
