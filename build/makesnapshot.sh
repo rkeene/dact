d3fc6087634af7abcbb8ebbde83064a64d448b4e
rkeene <rkeene@noemail.net>
2013-02-26 03:09:05 +0000

    Deleted legacy scripts
    FossilOrigin-Name: b53a103b9a0ce1eed6b0361e236edd0002575ebe

--- build/makesnapshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/rkeene/devel/compression
-
-VER=`/usr/bin/grep "DACT_VER_" dact.h | /bin/cut -f 3 -d " " | /usr/bin/tr "\n" . | /bin/cut -f 1-3 -d "."`
-
-./makearch.sh 2>/dev/null >/dev/null
-
-cp ../dact-$VER.tar.gz /web/rkeene/devel/dact-snapshot.tar.gz
