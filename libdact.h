42b1649c8f710ffb4d7d7597a840437f470f77ab
rkeene <rkeene@noemail.net>
2013-03-05 03:08:12 +0000

    Removed much of the original source
    FossilOrigin-Name: 67a663abd190b392e01a60e120110ad55f54f7c4

--- libdact.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIBDACT_H
-#define LIBDACT_H
-
-int dact_init(void);
-void *dact_openfile(const char *pathname);
-int dact_BuffToBuffDecompress(void);
-int dact_BuffToBuffCompress(void);
-
-#endif
--- sfx.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DACT_SFX_H
-#define _DACT_SFX_H
-
-#include "dact.h"
-
-ssize_t sfx_init_compress(int fd);
-ssize_t sfx_init_decompress(int fd);
-
-#endif
